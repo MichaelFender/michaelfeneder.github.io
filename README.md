7d1d781c8ed0535bcbc03d55fb853c6dcdd8fa77
MichaelFeneder <michael.a.fender@outlook.com>
2021-12-02 16:35:47 -0600

    check

--- README.md
+++
@@ -1,2 +0,0 @@
-
-# michaelfeneder.github.io
---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const path = require("path");
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'))
+})
+
+const port = process.env.PORT || 4005;
+
+app.listen(port, () => {
+    console.log(`My app is PUMPIN on port number ${port}`);
+});
+
+
+
+
